78af6411924e1b3d7abf195e9b91d0c0d6e81fde
Johnny Olson <john.olson3@gmail.com>
2018-11-30 12:57:03 -0800

    Add project files.

---
+++ Acme.Common/OperationResult.cs
@@ -0,0 +1,38 @@
+﻿namespace Acme.Common
+{
+    /// <summary>
+    /// Provides a success flag and message 
+    /// useful as a method return type.
+    /// </summary>
+    public class OperationResult<T>
+    {
+        public OperationResult()
+        {
+        }
+
+        public OperationResult(T result, string message) : this()
+        {
+            this.Result = result;
+            this.Message = message;
+        }
+
+        public T Result { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class OperationResultDecimal
+    {
+        public OperationResultDecimal()
+        {
+        }
+
+        public OperationResultDecimal(decimal value, string message) : this()
+        {
+            this.Result = value;
+            this.Message = message;
+        }
+
+        public decimal Result { get; set; }
+        public string Message { get; set; }
+    }
+}
