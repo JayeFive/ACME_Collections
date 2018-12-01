95fec6a3d0d650a251faecd33fa271bc7e2b66e9
Johnny Olson <john.olson3@gmail.com>
2018-11-30 20:39:18 -0800

    add unit test to test SendMail

--- Acme.BizTests1/VendorTests.cs
+++ Acme.BizTests1/VendorTests.cs
@@ -26,5 +26,24 @@ namespace Acme.Biz.Tests
             //Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod()]
+        public void SendMailTest()
+        {
+            //Arrange
+            var vendorRepository = new VendorRepository();
+            var vendors = vendorRepository.Retrieve();
+            var expected = new List<string>()
+            {
+                "Message sent: Important message for: ABC Corp",
+                "Message sent: Important message for: XYZ Inc"
+            };
+
+            //Act
+            var actual = Vendor.SendEmail(vendors, "Test message");
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
     }
 }
