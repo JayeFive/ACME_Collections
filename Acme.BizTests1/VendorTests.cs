a5684a8a401c1e1a765c939557e56227eae87d9f
Johnny Olson <john.olson3@gmail.com>
2018-11-30 20:35:59 -0800

    add ToStringTest to test for console output syntax

---
+++ Acme.BizTests1/VendorTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class VendorTests
+    {
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            //Arrange
+            var vendor = new Vendor();
+            vendor.VendorId = 1;
+            vendor.CompanyName = "ABC Corp";
+            var expected = "Vendor: ABC Corp (1)";
+
+            //Act
+            var actual = vendor.ToString();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
