ddecfff0423065162be764d14abd3f9bc52decb7
Johnny Olson <john.olson3@gmail.com>
2018-12-03 10:10:22 -0800

    fix company name to appease unit test

--- Acme.Biz/VendorRepository.cs
+++ Acme.Biz/VendorRepository.cs
@@ -45,7 +45,7 @@ namespace Acme.Biz
             var vendors = new Vendor[2]
             {
                 new Vendor(){ VendorId = 1, CompanyName = "ABC Corp", Email = "abc@abc.com" },
-                new Vendor(){ VendorId = 2, CompanyName = "XYZ Corp", Email = "xyz@xyz.com" },
+                new Vendor(){ VendorId = 2, CompanyName = "XYZ Inc", Email = "xyz@xyz.com" },
             };
 
             return vendors;
